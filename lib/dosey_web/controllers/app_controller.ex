4c838bac2327e12607ce8ef2ec2cb823ecf240af
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2026-08-22 10:44:10 +0200

    Remove leftovers

--- lib/dosey_web/controllers/app_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule DoseyWeb.AppController do
-  use DoseyWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, :index)
-  end
-end
--- lib/dosey_web/controllers/app_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule DoseyWeb.AppHTML do
-  use DoseyWeb, :html
-
-  embed_templates "app_html/*"
-end
